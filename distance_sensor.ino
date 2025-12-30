b4ad5294ed4dea3891a2adba27a69e15a3cb712d
Christine Shim <christine.shim1123@gmail.com>
2025-12-29 19:16:47 -0800

    Update distance_sensor.ino

--- distance_sensor.ino
+++ distance_sensor.ino
@@ -4,14 +4,11 @@ int buzzPin = 2;
 float pingTravelTime;
 
 void setup() {
-  // put your setup code here, to run once:
-
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   pinMode(buzzPin, OUTPUT);
 }
 void loop() {
-  // 1. Get initial distance
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
@@ -21,9 +18,7 @@ void loop() {
   pingTravelTime = pulseIn(echoPin, HIGH);
   float distance = pingTravelTime * 0.0343 / 2;
   
-
-  // 2. The Loop
-  while(distance < 30 && distance > 0) { // Added > 0 because pulseIn returns 0 on error
+  while(distance < 30 && distance > 0) { 
     digitalWrite(buzzPin, HIGH);
     
     digitalWrite(trigPin, LOW);
